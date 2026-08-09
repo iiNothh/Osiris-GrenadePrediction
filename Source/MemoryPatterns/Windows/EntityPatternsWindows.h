418d34d428e1025bc6798f525974a5e1d364111b
unknown <kuzeyerkan2009@gmail.com>
2026-08-09 16:18:18 +0300

    Register entity fallback Windows patterns

--- Source/MemoryPatterns/Windows/EntityPatternsWindows.h
+++ Source/MemoryPatterns/Windows/EntityPatternsWindows.h
@@ -14,6 +14,8 @@ struct EntityPatterns {
             .template addPattern<OffsetToVData, CodePattern{"89 ? ? ? ? 48 85 C9 74 ? 48 8B 49 ? 48 8D"}.add(1).read()>()
             .template addPattern<OffsetToRenderComponent, CodePattern{"0F 84 ? ? ? ? 49 8B 8D ? ? ? ? 48 85 C9"}.add(9).read()>()
             .template addPattern<OffsetToOwnerEntity, CodePattern{"44 8B 81 ? ? ? ? 4C 8B DA"}.add(3).read()>()
+            .template addPattern<OffsetToViewOffset, CodePattern{"F3 0F 10 15 ? ? ? ? 48 81 C1 ? ? ? ? 33 D2 E9 ? ? ? ?"}.add(11).read()>()
+            .template addPattern<OffsetToAbsVelocity, CodePattern{"F2 0F 11 86 ? ? ? ? 8B 40 08 89 86 ? ? ? ? E9"}.add(4).read()>()
             .template addPattern<GetAbsOriginFunction, CodePattern{"F8 ? 75 ? E8 ? ? ? ? F3"}.add(5).abs()>();
     }
 };
