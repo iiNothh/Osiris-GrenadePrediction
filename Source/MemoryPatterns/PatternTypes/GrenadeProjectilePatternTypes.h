4746a1319ecec632164b39604f17f7d8603b81b9
unknown <kuzeyerkan2009@gmail.com>
2026-08-09 18:03:05 +0300

    Define projectile launch state types

---
+++ Source/MemoryPatterns/PatternTypes/GrenadeProjectilePatternTypes.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+
+#include <CS2/Classes/Entities/C_BaseCSGrenadeProjectile.h>
+#include <CS2/Classes/Entities/C_BaseGrenade.h>
+#include <Utils/FieldOffset.h>
+#include <Utils/StrongTypeAlias.h>
+
+template <typename FieldType, typename OffsetType>
+using GrenadeProjectileOffset = FieldOffset<cs2::C_BaseCSGrenadeProjectile, FieldType, OffsetType>;
+template <typename FieldType, typename OffsetType>
+using GrenadeOffset = FieldOffset<cs2::C_BaseGrenade, FieldType, OffsetType>;
+
+STRONG_TYPE_ALIAS(OffsetToGrenadeInitialPosition, GrenadeProjectileOffset<cs2::C_BaseCSGrenadeProjectile::m_vInitialPosition, std::int32_t>);
+STRONG_TYPE_ALIAS(OffsetToGrenadeInitialVelocity, GrenadeProjectileOffset<cs2::C_BaseCSGrenadeProjectile::m_vInitialVelocity, std::int32_t>);
+STRONG_TYPE_ALIAS(OffsetToGrenadeThrower, GrenadeOffset<cs2::C_BaseGrenade::m_hThrower, std::int32_t>);
