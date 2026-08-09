b5f8b7a6b0c1e6199868468de481d4f73b3f55bb
unknown <kuzeyerkan2009@gmail.com>
2026-08-09 17:42:40 +0300

    Register model collision Windows pattern

--- Source/MemoryPatterns/Windows/BaseModelEntityPatternsWindows.h
+++ Source/MemoryPatterns/Windows/BaseModelEntityPatternsWindows.h
@@ -7,6 +7,7 @@ struct BaseModelEntityPatterns {
     [[nodiscard]] static consteval auto addClientPatterns(auto clientPatterns) noexcept
     {
         return clientPatterns
-            .template addPattern<OffsetToGlowProperty, CodePattern{"90 ? ? ? ? ? 8B ? 48 8D 88 ? ? ? ?"}.add(11).read()>();
+            .template addPattern<OffsetToGlowProperty, CodePattern{"90 ? ? ? ? ? 8B ? 48 8D 88 ? ? ? ?"}.add(11).read()>()
+            .template addPattern<OffsetToCollisionProperty, CodePattern{"80 B9 ? ? ? ? ? 48 8D B9 ? ? ? ? 48 8B D9 74 ? 48 8B CF E8 ? ? ? ? 48 8B CF 48 8B D8 E8 ? ? ? ?"}.add(10).read()>();
     }
 };
