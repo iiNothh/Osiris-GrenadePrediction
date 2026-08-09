38a62a3317eba5730f1c8198134f6ad9bd966758
unknown <kuzeyerkan2009@gmail.com>
2026-08-09 18:03:05 +0300

    Define projectile launch state types

--- Source/CS2/Classes/Entities/C_BaseCSGrenadeProjectile.h
+++ Source/CS2/Classes/Entities/C_BaseCSGrenadeProjectile.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <CS2/Classes/Vector.h>
+
 #include "C_BaseGrenade.h"
 
 namespace cs2
 {
 
 struct C_BaseCSGrenadeProjectile : C_BaseGrenade {
+    using m_vInitialPosition = Vector;
+    using m_vInitialVelocity = Vector;
 };
 
 }
--- Source/CS2/Classes/Entities/C_BaseGrenade.h
+++ Source/CS2/Classes/Entities/C_BaseGrenade.h
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <CS2/Classes/EntitySystem/CEntityHandle.h>
+
 #include "C_BaseModelEntity.h"
 
 namespace cs2
 {
 
+struct C_CSPlayerPawn;
+
 struct C_BaseGrenade : C_BaseModelEntity {
+    using m_hThrower = CHandle<C_CSPlayerPawn>;
 };
 
 }
